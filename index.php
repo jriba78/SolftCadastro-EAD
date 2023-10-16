7b810ade163bbdfd86f1c1ef29b493daf0d3af21
Jose Ribamar Vieira Batista <jriba78@gmail.com>
2023-10-15 23:29:55 -0300

    Update and rename b.txt to index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+ <!DOCTYPE html>
+ <html  long="pt-br">
+	  <head>
+		 <meta chart="utf-8">
+		 <title>CADASTRO</title>  
+		 
+	  </head>
+	 <body> 
+		<h1> CADASTRO DE CLIENTE </h1>
+		<form method="POST" action="painel.php">
+			<label>Name: </label>
+			<input type="text" name="nome" placeholder="Digite o seu nome completo"><br><br>
+
+			<label>E-mail: </label>
+			<input type="email" name="email" placeholder="Digite o seu email"><br><br>
+			
+			<input type="submit" value="Cadastrar">
+		</form>
+			
+	 </body>
+ </html>
+      
