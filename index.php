7349cae2a20b9fc2e46e4f7fffaed95c7dafed10
Jose Ribamar Vieira Batista <jriba78@gmail.com>
2023-11-06 20:45:25 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+
+?>
+ 
  <!DOCTYPE html>
  <html  long="pt-br">
 	  <head>
@@ -6,7 +11,16 @@
 		 
 	  </head>
 	 <body> 
+	    <a href="index.php">CADASTRO</a><br>
+		<a href="listar.php">Listar</a><br>
 		<h1> CADASTRO DE CLIENTE </h1>
+<?php
+if (isset($_SESSION['msg'])){
+
+	echo ($_SESSION['msg']);
+	unset($_SESSION['msg']);
+}
+?>
 		<form method="POST" action="painel.php">
 			<label>Name: </label>
 			<input type="text" name="nome" placeholder="Digite o seu nome completo"><br><br>
