b3aa5439075928fb93f3b34334f095fbd958816c
Jose Ribamar Vieira Batista <jriba78@gmail.com>
2023-11-06 20:47:56 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,16 +1,9 @@
 <?php
 $servidor = "localhost";
-$usuario = "root";
+$usuario= "root";
 $senha = "";
 $dbname = "bdCliente";
 
 # Criar conexão
 
-$mysqli = new mysqli($servidor, $usuario, $senha, $dbname);
-
-if ($mysqli->connect_error) {
-    
-   
-die("Falha ao conectar: " . $mysqli->connect_error);
-}
-?>
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
